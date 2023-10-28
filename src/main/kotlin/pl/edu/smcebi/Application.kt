459194bbbcef0898363390c82a3bad7bde0bd73c
Seba0855 <sebastian08555@gmail.com>
2023-10-28 23:22:54 +0200

    Konfiguracja Gradle

--- src/main/kotlin/pl/edu/smcebi/Application.kt
+++ src/main/kotlin/pl/edu/smcebi/Application.kt
@@ -5,9 +5,6 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
-import pl.edu.smcebi.plugins.configureRouting
-import pl.edu.smcebi.plugins.configureSecurity
-import pl.edu.smcebi.plugins.configureSerialization
 
 fun main() {
     embeddedServer(Netty, port = 2137, host = "0.0.0.0", module = Application::module)
