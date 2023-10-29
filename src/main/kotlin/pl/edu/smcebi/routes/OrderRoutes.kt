8486df44869f675d7540815d8a27d5b8c8bc3341
JuliaCaputa <julia.caputa@giantlazer.com>
2023-10-29 18:21:18 +0100

    Added NewOrder endpoint

--- src/main/kotlin/pl/edu/smcebi/routes/OrderRoutes.kt
+++ src/main/kotlin/pl/edu/smcebi/routes/OrderRoutes.kt
@@ -17,3 +17,24 @@ fun Route.listOrdersRoute() {
         }
     }
 }
+
+fun Route.createNewOrder() {
+    post("/order/new") {
+        val orderItems = call.receive<List<OrderItem>>()
+        val newOrder = orderItems.toNewOrder()
+        orderStorage.add(newOrder)
+        call.respondText(
+            "Zamówienie o ID ${newOrder.number} zostało pomyślnie złożone",
+            status = HttpStatusCode.Created
+        )
+    }
+}
+
+private fun List<OrderItem>.toNewOrder() = Order(
+    number = "${getCurrentDate()}-${UUID.randomUUID()}",
+    contents = this
+)
+
+private fun getCurrentDate(): String = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_PATTERN))
+
+const val DATE_PATTERN = "yyyy-MM-dd"
